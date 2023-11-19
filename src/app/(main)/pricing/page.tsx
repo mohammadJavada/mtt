24230bec14744033e4eba4ceb8c39dd6c0171c2e
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-19 14:42:35 +0330

    skip

--- src/app/(main)/pricing/page.tsx
+++ src/app/(main)/pricing/page.tsx
@@ -1,26 +1,18 @@
 // import GetStaticDatasAPI from "@/apis/static-datas/get-static-data.api";
-// import Pricing from "@/page/pricing";
+import Pricing from "@/page/pricing";
 
-// const PricingPage = async () => {
-//   let postedData = {
-//     page_number: 1,
-//     page_size: 200,
-//   };
-//   const brandData = await GetStaticDatasAPI({
-//     endPoint: "/BrandModelType/Get/All",
-//     data: postedData,
-//     method: "post",
-//   });
+const PricingPage = async () => {
+  // let postedData = {
+  //   page_number: 1,
+  //   page_size: 200,
+  // };
+  // const brandData = await GetStaticDatasAPI({
+  //   endPoint: "/BrandModelType/Get/All",
+  //   data: postedData,
+  //   method: "post",
+  // });
 
-//   return <Pricing brandModel={brandData?.brandModelTypes ?? []} />;
-// };
-
-// export default PricingPage;
-
-import React from "react";
-
-const page = () => {
-  return <div>pricing</div>;
+  return <Pricing brandModel={[]} />;
 };
 
-export default page;
+export default PricingPage;
