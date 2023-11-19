5097926c199852dc3a7fbff7c072005e4e988734
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-19 16:34:15 +0330

    skiofredfr

--- src/app/(main)/pricing/steps/page.tsx
+++ src/app/(main)/pricing/steps/page.tsx
@@ -1,19 +1,18 @@
 import GetStaticDatasAPI from "@/apis/static-datas/get-static-data.api";
-import GetStaticDatasNotSSRAPI from "@/apis/static-datas/get-static-datas-not-ssr.api";
 import { FRONT2DB } from "@/config/url";
 import PricingSteps from "@/page/pricing/steps";
 
-// async function postData(url = "", data = {}) {
-//   const response = await fetch(url, {
-//     method: "POST",
-//     cache: "force-cache",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify(data),
-//   });
-//   return response.json();
-// }
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    cache: "force-cache",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
 
 const PricingStepPage = async () => {
   let postedData = {
@@ -21,14 +20,9 @@ const PricingStepPage = async () => {
     page_size: 200,
   };
 
-  // const data = await postData(`${FRONT2DB}/BrandModelType/Get/All`, postedData);
-  const brandData = await GetStaticDatasNotSSRAPI({
-    endPoint: "/BrandModelType/Get/All",
-    data: postedData,
-    method: "post",
-  });
+  const data = await postData(`${FRONT2DB}/BrandModelType/Get/All`, postedData);
 
-  return <PricingSteps brandModel={brandData?.brandModelTypes || []} />;
+  return <PricingSteps brandModel={data?.brandModelTypes || []} />;
 };
 
 export default PricingStepPage;
