87dd7e30d5341b17405ed1d11478f0f1a6c4811f
mohammadjavad <mohammadjavada78@gmail.com>
2023-11-19 15:10:05 +0330

    use fetch

--- src/app/(main)/pricing/steps/page.tsx
+++ src/app/(main)/pricing/steps/page.tsx
@@ -1,17 +1,39 @@
 import GetStaticDatasAPI from "@/apis/static-datas/get-static-data.api";
+import { FRONT2DB } from "@/config/url";
 import PricingSteps from "@/page/pricing/steps";
+
+async function postData(url = "", data = {}) {
+  const response = await fetch(url, {
+    method: "POST",
+    cache: "no-store",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return response.json();
+}
 
 const PricingStepPage = async () => {
   let postedData = {
     page_number: 1,
     page_size: 200,
   };
+
+  const data = await postData(`${FRONT2DB}/BrandModelType/Get/All`, postedData);
   // const brandData = await GetStaticDatasAPI({
   //   endPoint: "/BrandModelType/Get/All",
   //   data: postedData,
   //   method: "post",
   // });
-  return <PricingSteps brandModel={[]} />;
+
+  // const getList = async () => {
+  //   const res = fetch("/BrandModelType/Get/All", {
+  //     method: "post",
+  //     cache: "no-store",
+  //   });
+  // };
+  return <PricingSteps brandModel={data?.brandModelTypes ?? []} />;
 };
 
 export default PricingStepPage;
